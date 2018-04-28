d4334c5f74887a112867c1bc9214c74656ade9ba
mfyanga <yamf1@163.com>
2018-04-28 09:52:17 +0800

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,15 @@
+/*********************************************************
+* Copyright 2018 The mfyanga Authors. All Rights Reserved.
+*FileName:Test.cpp
+*Description:jsut for test use git
+*author: mfyanga    date:2018-04-28
+*
+*********************************************************/
+#include<iostream>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    cout << "just for test use git" << endl;
+    return 0;
+}
